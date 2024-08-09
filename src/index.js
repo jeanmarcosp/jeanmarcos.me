9ec13985fb7a385f1168da8d610c91f7be6cdaa0
jeanmarcosp <jeanmarcos.perez.24@dartmouth.edu>
2024-08-09 10:04:23 -0400

    fix

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-import './style.scss';
-
-let seconds = 0;
-
-setInterval(() => {
-  seconds += 1;
-  $('#main').html(`You've been on this page for ${seconds} seconds.`);
-}, 1000);
