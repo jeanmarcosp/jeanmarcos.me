289591985dd1feea0d1353f8b3ffff920c679618
jeanmarcosp <jeanmarcos.perez.24@dartmouth.edu>
2024-08-09 10:20:31 -0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import eslint from 'vite-plugin-eslint';
 import autoprefixer from 'autoprefixer';
-import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/jeanmarcos.me/',
   plugins: [
     eslint(),
-    // vitePluginFaviconsInject(),
   ],
   css: {
     postcss: {
