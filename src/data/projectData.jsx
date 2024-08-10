67f34b49860d5e784cfd5d1c211deccaf67a9a8e
jeanmarcosp <jeanmarcos.perez.24@dartmouth.edu>
2024-08-10 15:07:40 -0400

    added projects

--- src/data/projectData.jsx
+++ src/data/projectData.jsx
@@ -4,7 +4,7 @@ const projectData = [
     langauge: "C",
     title: "Tiny Search Engine",
     description:
-        "Developed a web crawler that retrieves and store webpages recursively, an indexes web document, and a handles search queries",
+        "Developed a web crawler that retrieves and store webpages recursively, an indexes web document, and a handles search queries.",
     link: "https://github.com/jeanmarcosp/tiny-search-engine",
   },
   {
@@ -20,7 +20,7 @@ const projectData = [
     langauge: "JavaScript",
     title: "Personal Website",
     description:
-        "Created a personal website to showcase my projects and resume. Coded using React.js and styled with Mantine component library",
+        "Created a personal website to showcase my projects and resume. Coded using React.js and styled with Mantine component library.",
     link: "https://github.com/jeanmarcosp/jeanmarcos.me",
   },
   {
@@ -36,8 +36,40 @@ const projectData = [
     langauge: "Python",
     title: "Seamless Copy & Paste",
     description:
-        "Used least sqaures and gradients to seamlessly blend an object or texture from a source image into a target image. ",
+        "Used least sqaures and gradients to seamlessly blend an object or texture from a source image into a target image.",
     link: "https://github.com/jeanmarcosp/seamless-copy-paste",
+  },
+  {
+    id: 6,
+    langauge: "JavaScript",
+    title: "Literary Hub",
+    description:
+        "Literary Hub is a Spotify x Tik Tok take on poetry that lets you read public domain poetry, learn about it, and curate your own collections of your favorite poems.",
+    link: "https://github.com/jeanmarcosp/literary-hub",
+  },
+  {
+    id: 7,
+    langauge: "Python",
+    title: "Computational Photography",
+    description:
+        "Course work including an image processing pipeline, an hdr imaging pipeline, lightfield and depth from focus, and photometric stero.",
+    link: "https://github.com/jeanmarcosp/computational-photography",
+  },
+  {
+    id: 8,
+    langauge: "Python",
+    title: "Machine Learning & Data Analysis",
+    description:
+        "Course work including gradient descent, regression, naïve bayes, binary classfication and sentiment analysis.",
+    link: "https://github.com/jeanmarcosp/machine-learning-data-analysis",
+  },
+  {
+    id: 9,
+    langauge: "JavaScript",
+    title: "Biblio",
+    description:
+        "A community book exchange whose main goals are to help users save money, encourage reading, reduce overconsumption, and build community",
+    link: "https://github.com/jeanmarcosp/biblio-frontend",
   },
 ];
 
